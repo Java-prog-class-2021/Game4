527570deafdda04bee2ea55518f3b4e07e8a9375
Josh <joshmuszka67@gmail.com>
2021-12-16 09:31:24 -0500

    start game

---
+++ src/zombiegame/Bullet.java
@@ -0,0 +1,20 @@
+package zombiegame;
+
+public class Bullet {
+
+	int width = 8;
+	double height = 8;
+	double posX = 8;
+	double posY;
+	double speedX;
+	double speedY;
+	
+	Bullet(double posX, double posY, double speedX, double speedY) {
+		
+		this.posX = posX;
+		this.posY = posY;
+		this.speedX = speedX;
+		this.speedY = speedY;
+	}
+	
+}
