06ff65b7a8858d00bfe5183bfd65337fffb6bdb1
henrylee746 <96128004+henrylee746@users.noreply.github.com>
2022-01-20 22:09:41 -0500

    Update Zombie.java

--- src/zombiegame/Zombie.java
+++ src/zombiegame/Zombie.java
@@ -1,4 +1,11 @@
 package zombiegame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import java.awt.Image;
 
 public class Zombie {
 
@@ -12,9 +19,30 @@ public class Zombie {
 	double damage = 8;
 	double fullHealth = 50;
 	double health = fullHealth;
+	int zombieW;
+	int zombieH;
+	
+	Image imgZombie;
 	
 	Zombie() {
-		
+		imgZombie = loadImage("zombie1.png");
+		zombieW = imgZombie.getWidth(null);
+		zombieH = imgZombie.getWidth(null);
 	}
-	
+	Image loadImage(String filename) {
+		Image image = null;
+		URL imageURL = this.getClass().getResource("/" + filename);
+		if (imageURL != null) {
+			ImageIcon icon = new ImageIcon(imageURL);
+			image = icon.getImage();
+		} else {
+			JOptionPane.showMessageDialog(null, "An image failed to load: " + filename , "ERROR", JOptionPane.ERROR_MESSAGE);
+		}
+		return image;
+	}
+	public void draw(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+		if (imgZombie == null) return;
+		g.drawImage(imgZombie, (int)posX, (int)posY, zombieW, zombieH, null);
+	}
 }
