6c6de92cd4109deab91d78e913ccabe560658d3c
apemanjosh67 <76886612+apemanjosh67@users.noreply.github.com>
2022-01-22 18:36:01 -0500

    Update Border.java
    modified borders to have camera on the center of the map

--- src/zombiegame/Border.java
+++ src/zombiegame/Border.java
@@ -2,10 +2,10 @@ package zombiegame;
 
 public class Border {
 
-	double x = -1050+400;
-	double y = -1050+300;
-	double width = 2100;
-	double height = 2100;
+	double x = 400-1100;
+	double y = 300-1100;
+	double width = 2200;
+	double height = 2200;
 	
 	Border() {
 		
