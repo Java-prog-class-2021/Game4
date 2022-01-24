b536b9508e163bfa8376b581de75e696ff6bdcc6
apemanjosh67 <76886612+apemanjosh67@users.noreply.github.com>
2022-01-24 08:56:45 -0500

    Update and rename Building.java to Obstacle.java
    renamed from building to obstacle

--- src/zombiegame/Obstacle.java
+++ src/zombiegame/Obstacle.java
@@ -2,7 +2,7 @@ package zombiegame;
 
 import java.awt.Color;
 
-public class Building {
+public class Obstacle {
 
 	double x;
 	double y;
@@ -10,7 +10,7 @@ public class Building {
 	double height;
 	Color colour;
 	
-	Building (double x, double y, double width, double height, Color colour) {
+	Obstacle (double x, double y, double width, double height, Color colour) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
