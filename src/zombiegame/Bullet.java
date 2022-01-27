da256b6e18fbaa646e656b8ee2c2e1ff82435816
henrylee746 <96128004+henrylee746@users.noreply.github.com>
2022-01-27 13:47:18 -0500

    Update Bullet.java

--- src/zombiegame/Bullet.java
+++ src/zombiegame/Bullet.java
@@ -4,18 +4,17 @@ public class Bullet {
 
 	int width = 6;
 	double height = 6;
-	double posX = 8;
-	double posY;
+	double x;
+	double y;
 	double speedX;
 	double speedY;
 	double damage = 20;
 	
-	Bullet(double posX, double posY, double speedX, double speedY) {
+	Bullet(double x, double y, double speedX, double speedY) {
 		
-		this.posX = posX;
-		this.posY = posY;
+		this.x = x;
+		this.y = y;
 		this.speedX = speedX;
 		this.speedY = speedY;
 	}
-	
 }
